47ecfb4da9f20e5c266debac9dcb212bbcd3c564
JZachrisson <jesper.zachrisson@gmail.com>
2021-02-16 11:06:09 +0100

    initital commit

---
+++ src/components/List/List.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const list = [
+  {
+    author: 'Jon Fingas',
+    title: 'Tesla buys $1.5 in Bitcoin, will soon accept it as payment',
+    url:
+      'https://www.endgadget.com/tesla-to-take-bitcoin-payments-140109988.html',
+    publishedAt: '2021-02-08',
+  },
+  {
+    author: 'Gregory Barber',
+    title: 'Would you trade a Bitcoin for a Tesla?',
+    url: 'https://www.wired.com/story/would-you-trade-bitcoin-tesla',
+    publishedAt: '2021-02-09',
+  },
+];
+
+const List = () => {
+  return <div></div>;
+};
+
+export default List;
